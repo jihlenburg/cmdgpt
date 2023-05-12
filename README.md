ef8965ddfa4030cf1718a347036449d93840a0f9
Joern Ihlenburg <ihlenburg@ihlems.de>
2023-05-12 18:30:50 +0200

    Additional fixes.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # CmdGpt
 
-CmdGpt is a command-line tool for interacting with the OpenAI GPT-4 API.
+This is a command-line tool to interact with the OpenAI GPT API. It allows you to send prompts and receive responses from the GPT model.
 
 ## Prerequisites
 
@@ -39,7 +39,39 @@ The resulting `cmdgpt` executable will be in the `build` directory.
 
 ## Usage
 
-You can run the `cmdgpt` tool with the following command:
+cmdgpt [options] prompt
 
-```sh
-./cmdgpt [prompt] [-k api_key] [-s system_prompt]
+Options:
+
+-h, --help: Show this help message
+-k, --api_key: API key for OpenAI GPT API
+-s, --sys_prompt: System prompt for OpenAI GPT API
+-l, --log_file: Logfile to log messages
+-m, --gpt_model: GPT model to use (default: gpt-4)
+-L, --log_level: Log level (TRACE, DEBUG, INFO, WARN, ERROR, CRITICAL). Default is WARN.
+
+## Environment Variables
+
+The following environment variables can also be used to set the corresponding parameters:
+
+OPENAI_API_KEY: API key for OpenAI GPT API
+OPENAI_SYS_PROMPT: System prompt for OpenAI GPT API
+CMDGPT_LOG_FILE: Logfile to log messages
+OPENAI_GPT_MODEL: GPT model to use
+CMDGPT_LOG_LEVEL: Log level
+If both a command-line option and an environment variable are provided, the command-line option takes precedence.
+
+## Exit Status Codes
+
+The tool uses the following exit status codes:
+
+0: Success
+64: Command-line usage error
+78: Configuration error
+75: Temporary failure
+1: Other errors
+
+## Note
+
+Note: The `-L` option expects a log level in uppercase. The `CMDGPT_LOG_LEVEL` environment variable also expects an uppercase log level. If an invalid log level is provided, the default log level (WARN) will be used.
+
--- cmdgpt.cpp
+++ cmdgpt.cpp
@@ -1,167 +1,244 @@
-#include <iostream>             // for std::cout, std::cerr
-#include <string>               // for std::string
-#include <stdexcept>            // for std::invalid_argument
-#include <cstdlib>              // for std::getenv
-#include "httplib.h"            // for httplib::Client
-#include "nlohmann/json.hpp"    // for json
+#include <iostream>
+#include <string>
+#include <stdexcept>
+#include <cstdlib>
+#include <fstream>
+#include "httplib.h"
+#include "nlohmann/json.hpp"
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/ansicolor_sink.h"
+#include "spdlog/sinks/file_sinks.h"
 
 using json = nlohmann::json;
 
+// Preprocessor defines for constants
+#define DEFAULT_MODEL "gpt-4"
+#define DEFAULT_SYSTEM_PROMPT "You are a helpfull assitant!"
+#define DEFAULT_LOG_LEVEL spdlog::level::warn
+#define AUTHORIZATION_HEADER "Authorization"
+#define CONTENT_TYPE_HEADER "Content-Type"
+#define APPLICATION_JSON "application/json"
+#define SYSTEM_ROLE "system"
+#define USER_ROLE "user"
+#define MODEL_KEY "model"
+#define MESSAGES_KEY "messages"
+#define ROLE_KEY "role"
+#define CONTENT_KEY "content"
+#define CHOICES_KEY "choices"
+#define FINISH_REASON_KEY "finish_reason"
+#define URL "/v1/chat/completions"
+#define SERVER_URL "https://api.openai.com"
+#define EMPTY_RESPONSE_CODE -1
+#define HTTP_OK 200
+#define HTTP_BAD_REQUEST 400
+#define HTTP_UNAUTHORIZED 401
+#define HTTP_FORBIDDEN 403
+#define HTTP_NOT_FOUND 404
+#define HTTP_INTERNAL_SERVER_ERROR 500
+
+// Map of string log levels to spdlog::level::level_enum values
+const std::map<std::string, spdlog::level::level_enum> log_levels = {
+    {"TRACE", spdlog::level::trace},
+    {"DEBUG", spdlog::level::debug},
+    {"INFO", spdlog::level::info},
+    {"WARN", spdlog::level::warn},
+    {"ERROR", spdlog::level::err},
+    {"CRITICAL", spdlog::level::critical},
+};
+
+// Global logger variable accessible by all functions
+std::shared_ptr<spdlog::logger> gLogger;
+
 /**
- * @brief Send a message to the GPT Chat API and return the HTTP response status code.
+ * @brief Prints the help message to the console.
+ */
+void print_help() {
+    std::cout << "Usage: ./program [options] [prompt]\n"
+              << "Options:\n"
+              << "  -h, --help              Show this help message and exit\n"
+              << "  -k, --api_key KEY       Set the OpenAI API key to KEY\n"
+              << "  -s, --sys_prompt PROMPT Set the system prompt to PROMPT\n"
+              << "  -l, --log_file FILE     Set the log file to FILE\n"
+              << "  -m, --gpt_model MODEL   Set the GPT model to MODEL\n"
+              << "  -L, --log_level LEVEL   Set the log level to LEVEL\n"
+              << "                          (TRACE, DEBUG, INFO, WARN, ERROR, CRITICAL)\n"
+              << "prompt:\n"
+              << "  The text prompt to send to the OpenAI GPT API. If not provided, the program will read from stdin.\n";
+}
+
+/**
+ * @brief Sends a message to the GPT Chat API and returns the HTTP response status code.
  * @param prompt The text prompt to send to the API.
  * @param response A reference to a string where the API response will be stored.
  * @param api_key The API key for the OpenAI GPT API. Default is an empty string.
  * @param system_prompt The system prompt for the OpenAI GPT API. Default is an empty string.
- * @return The HTTP response status code, or -1 if no response was received.
+ * @param model The GPT model to use. Default is DEFAULT_MODEL.
+ * @return The HTTP response status code, or EMPTY_RESPONSE_CODE if no response was received.
  * @throws std::invalid_argument If no API key was provided.
  */
-int get_gpt_chat_response(const std::string& prompt, std::string& response, const std::string& api_key = "", const std::string& system_prompt = "") {
-    // If no API key or system prompt is provided, throw an exception
+int get_gpt_chat_response(const std::string& prompt, std::string& response, const std::string& api_key = "", const std::string& system_prompt = "", const std::string& model = DEFAULT_MODEL) {
+    // Declare the required variables at the beginning of the function
+    json res_json;
+    std::string finish_reason;
+
+    // API key and system prompt must be provided
     if (api_key.empty() || system_prompt.empty()) {
         throw std::invalid_argument("API key and system prompt must be provided.");
     }
 
-    // Headers for the API request
+    // Setup headers for the POST request
     httplib::Headers headers = {
-        { "Authorization", "Bearer " + api_key },
-        { "Content-Type", "application/json" }
+        { AUTHORIZATION_HEADER, "Bearer " + api_key },
+        { CONTENT_TYPE_HEADER, APPLICATION_JSON }
     };
 
-    // Data to be sent in the API request
+    // Prepare the JSON data for the POST request
     json data = {
-        {"model", "gpt-4"},
-        {"messages", {
-            {{"role", "system"}, {"content", system_prompt}},
-            {{"role", "user"}, {"content", prompt}}
+        {MODEL_KEY, model},
+        {MESSAGES_KEY, {
+            {{ROLE_KEY, SYSTEM_ROLE}, {CONTENT_KEY, system_prompt}},
+            {{ROLE_KEY, USER_ROLE}, {CONTENT_KEY, prompt}}
         }}
     };
 
-    // Use chat endpoint
-    std::string url = "/v1/chat/completions";
+    // Initialize the HTTP client
+    httplib::Client cli(SERVER_URL);
 
-    // Send the post request to the API
-    httplib::Client cli("https://api.openai.com");
+    // Log the data being sent
+    gLogger->debug("Debug: Sending POST request to {} with data: {}", URL, data.dump());
 
-    std::cerr << "Debug: Sending POST request to " << url << " with data: " << data.dump() << std::endl;
-    auto res = cli.Post(url.c_str(), headers, data.dump(), "application/json");
+    // Send the POST request
+    auto res = cli.Post(URL, headers, data.dump(), APPLICATION_JSON);
+
+    // If response is received from the server
     if (res) {
-        std::cerr << "Debug: Received HTTP response with status " << res->status << " and body: " << res->body << std::endl;
+        gLogger->debug("Debug: Received HTTP response with status {} and body: {}", res->status, res->body);
+        // Handle the HTTP response status code
+        switch (res->status) {
+            case HTTP_OK:
+                // Everything is fine
+                break;
+            case HTTP_BAD_REQUEST:
+                gLogger->error("Error: Bad request.");
+                return res->status;
+            case HTTP_UNAUTHORIZED:
+                gLogger->error("Error: Unauthorized. Check your API key.");
+                return res->status;
+            case HTTP_FORBIDDEN:
+                gLogger->error("Error: Forbidden. You do not have the necessary permissions.");
+                return res->status;
+            case HTTP_NOT_FOUND:
+                gLogger->error("Error: Not Found. The requested URL was not found on the server.");
+                return res->status;
+            case HTTP_INTERNAL_SERVER_ERROR:
+                gLogger->error("Error: Internal Server Error. The server encountered an unexpected condition.");
+                return res->status;
+            default:
+                gLogger->error("Error: Received unexpected HTTP status code: {}", res->status);
+                return res->status;
+        }
     } else {
-        std::cerr << "Debug: No response received from the server." << std::endl;
-    }   
-
-    // If response is received, process it
-    if (res && !res->body.empty()) {
-        json res_json = json::parse(res->body);
-        if (res_json["choices"].empty()) {
-            std::cerr << "Error: 'choices' array is empty." << std::endl;
-            return -1;  // or some other error code
-        }
-        if (!res_json["choices"][0].contains("finish_reason")) {
-            std::cerr << "Error: 'finish_reason' field is missing." << std::endl;
-            return -1;  // or some other error code
-        }
-        if (!res_json["choices"][0]["message"].contains("content")) {
-            std::cerr << "Error: 'content' field is missing." << std::endl;
-            return -1;  // or some other error code
-        }
-        std::string finish_reason = res_json["choices"][0]["finish_reason"].get<std::string>();
-        std::cerr << "Finish reason: " << finish_reason << std::endl;
-        response = res_json["choices"][0]["message"]["content"].get<std::string>();
+        gLogger->debug("Debug: No response received from the server.");
+        return EMPTY_RESPONSE_CODE;
     }
-    
-    // Return the status code (or -1 if no response was received)
-    return res ? res->status : -1;
+
+    // If response body is not empty
+    if (!res->body.empty()) {
+        // Parse the JSON response
+        res_json = json::parse(res->body);
+
+        // If 'choices' array is empty
+        if (res_json[CHOICES_KEY].empty()) {
+            gLogger->error("Error: '{}' array is empty.", CHOICES_KEY);
+            return EMPTY_RESPONSE_CODE;
+        }
+        // If 'finish_reason' field is missing
+        if (!res_json[CHOICES_KEY][0].contains(FINISH_REASON_KEY)) {
+            gLogger->error("Error: '{}' field is missing.", FINISH_REASON_KEY);
+            return EMPTY_RESPONSE_CODE;
+        }
+        // If 'content' field is missing
+        if (!res_json[CHOICES_KEY][0]["message"].contains(CONTENT_KEY)) {
+            gLogger->error("Error: '{}' field is missing.", CONTENT_KEY);
+            return EMPTY_RESPONSE_CODE;
+        }
+
+        // Extract 'finish_reason' and 'content'
+        finish_reason = res_json[CHOICES_KEY][0][FINISH_REASON_KEY].get<std::string>();
+        gLogger->debug("Finish reason: {}", finish_reason);
+        response = res_json[CHOICES_KEY][0]["message"][CONTENT_KEY].get<std::string>();
+    }
+
+    return res->status;
 }
 
 /**
- * @brief Function to print help message.
- */
-void print_help() {
-    std::cout << "Usage: cmdgpt [options] prompt\n"
-              << "Options:\n"
-              << "-h, --help        Show this help message\n"
-              << "-k, --api_key     API key for OpenAI GPT API\n"
-              << "-s, --sys_prompt  System prompt for OpenAI GPT API\n";
-}
-
-/**
- * @brief Entry point of the application.
- * @param argc Number of command-line arguments.
- * @param argv Array of command-line arguments.
- * @return Exit status. 0 indicates success.
+ * @brief The main function of the application.
+ * @param argc The number of command-line arguments.
+ * @param argv The command-line arguments.
+ * @return The exit code of the application.
  */
 int main(int argc, char* argv[]) {
-    // Variables to hold command-line arguments
-    std::string prompt;
     std::string api_key;
-    std::string system_prompt; // Don't initialize here
+    std::string system_prompt;
+    std::string gpt_model;
+    std::string log_file;
+    spdlog::level::level_enum log_level;
+    std::string arg;
+    std::string prompt;
+    std::string response;
+    int status_code;
 
-    // Handle command-line arguments
+    // Parse command-line arguments and environment variables
+    api_key = getenv("OPENAI_API_KEY") ? getenv("OPENAI_API_KEY") : "";
+    system_prompt = getenv("OPENAI_SYSTEM_PROMPT") ? getenv("OPENAI_SYSTEM_PROMPT") : DEFAULT_SYSTEM_PROMPT;
+    gpt_model = getenv("OPENAI_GPT_MODEL") ? getenv("OPENAI_GPT_MODEL") : DEFAULT_MODEL;
+    log_file = getenv("CMDGPT_LOG_FILE") ? getenv("CMDGPT_LOG_FILE") : "logfile.txt"; // Default log file
+    std::string env_log_level = getenv("CMDGPT_LOG_LEVEL") ? getenv("CMDGPT_LOG_LEVEL") : "WARN"; // Default log level
+    log_level = log_levels.count(env_log_level) ? log_levels.at(env_log_level) : DEFAULT_LOG_LEVEL;
+    // Check for command-line options
+    // Parsing command-line arguments
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         if (arg == "-h" || arg == "--help") {
-            // If the help argument was passed, print the help message and exit
             print_help();
-            return 0;
+            return EXIT_SUCCESS;
         } else if (arg == "-k" || arg == "--api_key") {
-            // If this is the API key argument, use the next argument as the API key
             api_key = argv[++i];
         } else if (arg == "-s" || arg == "--sys_prompt") {
-            // If this is the system prompt argument, use the next argument as the system prompt
             system_prompt = argv[++i];
+        } else if (arg == "-l" || arg == "--log_file") {
+            log_file = argv[++i];
+        } else if (arg == "-m" || arg == "--gpt_model") {
+            gpt_model = argv[++i];
+        } else if (arg == "-L" || arg == "--log_level") {
+            std::string log_level_str = argv[++i];
+            if (log_levels.count(log_level_str)) {
+                log_level = log_levels.at(log_level_str);
+            }
         } else {
-            // If this is a positional argument, use it as the prompt
             prompt = arg;
         }
     }
 
-    // Try to get the API key from the environment if it's not provided
-    if (api_key.empty()) {
-        char* env_api_key = std::getenv("OPENAI_API_KEY");
-        if (env_api_key != nullptr) {
-            api_key = env_api_key;
-        }
+    // Set up logging
+    auto console_sink = std::make_shared<spdlog::sinks::ansicolor_stdout_sink_mt>();
+    auto file_sink = std::make_shared<spdlog::sinks::simple_file_sink_mt>(log_file, true);
+    gLogger = std::make_shared<spdlog::logger>("multi_sink", spdlog::sinks_init_list{console_sink, file_sink});
+    gLogger->set_level(log_level);
+
+    // Make the API request and handle the response
+    if (prompt.empty()) {
+        // If no prompt was provided in the command line, read it from stdin
+        std::getline(std::cin, prompt);
     }
-
-    // Try to get the system prompt from the environment if it's not provided
-    if (system_prompt.empty()) {
-        char* env_sys_prompt = std::getenv("OPENAI_SYS_PROMPT");
-        if (env_sys_prompt != nullptr) {
-            system_prompt = env_sys_prompt;
-        }
-        else {
-            system_prompt = "You are a helpful assistant.";
-        }
+    status_code = get_gpt_chat_response(prompt, response, api_key, system_prompt, gpt_model);
+    if (status_code == EMPTY_RESPONSE_CODE) {
+        gLogger->critical("Error: Did not receive a response from the server.");
+        return 1;
     }
-
-    // Try to get a response from the GPT Chat API
-    try {
-        std::string response;
-        int status = get_gpt_chat_response(prompt, response, api_key, system_prompt);
-        
-        // Print the response
-        std::cout << "Response: " << response << std::endl;
-
-        // Print the HTTP status
-        std::cerr << "HTTP request status: " << status << std::endl;
-
-        // Handle HTTP response status codes
-        if (status == 200) {
-            return 0; // 0: Success
-        } else if (status == 400 || status == 404) {
-            return 64; // 64: Command line usage error
-        } else if (status == 401 || status == 403) {
-            return 78; // 78: Configuration error
-        } else if (status == 503 || status == 504) {
-            return 75; // 75: Temporary failure
-        } else {
-            return 1; // 1: Other errors
-        }
-    } catch (const std::invalid_argument& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 22; // 22: Invalid argument
-    }
+    // output the response to stdout
+    std::cout << response << std::endl;
+    // that's all folks...
+    return 0;
 }
-
